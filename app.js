8fc9ad88e4949cd9520f585992499065dcfc9c96
Raymond Gregory <vinedsel@gmail.com>
2017-12-10 04:58:15 -0600

    Imported a couple of different fonts for body text and headlines.

--- app.js
+++ app.js
@@ -50,25 +50,34 @@ $(document).ready(function() {
 });
 
 
-// Filling Tables
+// Filling Tables - moot
 
-function makeTable(container, data) {
-  var table = $("<table/>").addClass('CSSTableGenerator');
-  $.each(data, function(rowIndex, r) {
-    var row = $("<tr/>");
-    $.each(r, function(colIndex, c) {
-      row.append($("<t" + (rowIndex == 0 ? "h" : "d") + "/>").text(c));
-    });
-    table.append(row);
-  });
-  return container.append(table);
+// function makeTable(container, data) {
+//   var table = $("<table/>").addClass('CSSTableGenerator');
+//   $.each(data, function(rowIndex, r) {
+//     var row = $("<tr/>");
+//     $.each(r, function(colIndex, c) {
+//       row.append($("<t" + (rowIndex == 0 ? "h" : "d") + "/>").text(c));
+//     });
+//     table.append(row);
+//   });
+//   return container.append(table);
+//
+//   $(document).ready(function() {
+//     var data = [
+//       ["City 1", "City 2", "City 3"], //headers
+//       ["New York", "LA", "Seattle"],
+//       ["Paris", "Milan", "Rome"],
+//       ["Pittsburg", "Wichita", "Boise"]
+//     ]
+//     var cityTable = makeTable($(document.body), data);
+//   });
 
-  $(document).ready(function() {
-    var data = [
-      ["City 1", "City 2", "City 3"], //headers
-      ["New York", "LA", "Seattle"],
-      ["Paris", "Milan", "Rome"],
-      ["Pittsburg", "Wichita", "Boise"]
-    ]
-    var cityTable = makeTable($(document.body), data);
-  });
+
+// Tax function
+
+function taxFunction(value2) {
+    getElementsByClassName('value2');
+    value2 *= .08 ;
+
+};
